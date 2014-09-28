81231f61c986dc9f1872f6b76f1624bff6f1f0e4
KlausRaynor <klaus.a.holder@gmail.com>
2014-09-28 19:19:34 -0400

    first commit

---
+++ brainzzz.php
@@ -0,0 +1,4 @@
+<?php 
+echo 'zombies love brainsss...';
+
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!Doctype HTML>
+<html>
+
+<head>
+
+<title>Problem Set 2</title>
+<link rel='stylesheet' type='text/css' href='style.css'>
+</head>
+<body>
+<?php require 'brainzzz.php'; ?>
+
+<div class='card'></div>
+</body>
+</html>
