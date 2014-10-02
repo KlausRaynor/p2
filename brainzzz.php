f712188f5ee5d0f89394c3ba03f3dab90d520538
KlausRaynor <klaus.a.holder@gmail.com>
2014-10-02 10:56:41 -0400

    a more complete (but not perfect) password generator.  deleted style.css because I am using bootstrap.  Added password field div to display password and added the foreach loop structure.

--- brainzzz.php
+++ brainzzz.php
@@ -1,8 +1,33 @@
 <?php 
 
-$cards = '';
-for($i = 0; $i < 10; $i++){
+$wordBank = array('Bob', 'Susan', 'Jerry', 'Alecia', 'Peter', 'John', 'LH', 'Jason', 'Amy', 'Klaus');
+$password = '';
+$i = '';
 
-
+foreach($_GET as $key => $value) {
+	
+	if($key == 'wordNumber'){
+		for($i = 0; $i < $value; $i++) {
+			$randomWord = array_rand($wordBank);
+			$password .= $wordBank[$randomWord];
+		}
+	}else if($key == 'numberRadio' && $value == 'y'){
+		
+			$numberArray = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+			$randomNumber = array_rand($numberArray);
+			$password .= $numberKey[$randomNumber];
+		} else if($key == 'symbolRadio' && $value == 'y') {
+		
+			$symbolArray = array('!', '#', '$', '%', '?', '~', '*');
+			$randomSymbol = array_rand($symbolArray);
+			$password .= $symbolKey[$randomSymbol];
+		} else {
+			#$password = 'Something went wrong!!';
+			break;
+		}
 }
 
+/*
+	$winner = array_rand($contestants);
+	$contestants[$winner] = 'Winner';
+*/
--- index.php
+++ index.php
@@ -10,22 +10,53 @@ ini_set('display_errors', 1);
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 <title>Problem Set 2</title>
-<!--<link rel='stylesheet' type='text/css' href='style.css'> -->
 </head>
 <body>
+<pre>
 <?php require 'brainzzz.php'; ?>
+
+<!--
+<form action ='index.php' method='GET' >
+	<input type='text' name='contestant0'><br>
+	<input type='text' name='contestant1'><br>
+	<input type='text' name='contestant2'><br>
+	<input type='text' name='contestant3'><br>
+
+	<input type='submit' value='Pick a Winner!'>
+</form>
+
+<h1>Contestants</h1> 
+<?php foreach($contestants as $key => $value): ?>
+    <?=$key?> is a <?=$value?> <br>
+<?php endforeach; ?>
+ -->
+
+
 <div class='container'>
 <h3>Secure Password Generator! </h3>
-<form>
+
+<!--HTML Form -->
+<form action='index.php' method='GET'>
+
 <label for = 'wordNumber'>How many words?</label>
 <input maxlength=1 type=text name='wordNumber' id='number_of_words' size=1> (Max 9)<br><br>
+
 <label for= 'endNumber'> Want numbers? </label> <br>
-<input class='check' type='checkbox' name='numberRadio' value='y'>H311 y34h! </input><br>
-<input class='check' type='checkbox' name='numberRadio' value='n'>Hell no!</input><br>
+<input class='check' type='radio' name='numberRadio' value='y'> H311 y34h! </input><br>
+<input class='check' type='radio' name='numberRadio' value='n'> Hell no!</input><br>
+
+<label for='endSymbols'>How about some Symbols?</label><br>
+<input class='check' type='radio' name='symbolRadio' value='y'> YES!!! </input><br>
+<input class='check' type='radio' name='symbolRadio' value='n'> nope.</input><br>
+
 <input class='submit' type='submit' class='submit_btn' value='Lets see it!'>
 </form>
-<div class='card'></div>
+
+<div class='password_box'><?=$password ?></div>
+
 
 </div>
+
+</pre>
 </body>
 </html>
